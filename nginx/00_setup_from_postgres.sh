10b7d288589232e142f3273e3c7312edf4ff938a
stageleft <sugano-github@elder-alliance.org>
2024-01-02 10:28:11 +0000

    add DELETE Interface

--- nginx/00_setup_from_postgres.sh
+++ nginx/00_setup_from_postgres.sh
@@ -9,3 +9,11 @@ done
 while ! node /node_apps/index.js ; do
   sleep 1; # wait for boot up of dbserver
 done
+
+# check and retry generating current settings
+find '/etc/nginx/conf.d/' -type f -name "*.sec" | while read -r f; do
+  if [ $(wc -c < ${f}) -eq 0 ]; then
+    sleep 1;
+    /usr/bin/bash -c "$0"
+  fi
+done
