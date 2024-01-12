508091268f89f14059af23e385791aa5a6da9a88
stageleft <sugano-github@elder-alliance.org>
2024-01-13 01:01:39 +0900

    webadmin起動時にPostgreSQLにデータを突っ込む設定を追加

--- webadmin/init.sh
+++ webadmin/init.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+# psql bootup waiting
+# webadmin_password / webadmin existence check
+# re-create webadmin_password / webadmin if removed
+while ! /usr/local/bin/node /app/pre_boot.js ; do
+    sleep 1;
+done
+
 while : ; do
     /usr/local/bin/node /app/index.js
     sleep 1;
