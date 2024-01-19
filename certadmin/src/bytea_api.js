2374ee4bd8500117654e4cf423fd9dc69f909314
stageleft <sugano-github@elder-alliance.org>
2024-01-19 19:21:57 +0900

    ファイルのアップロード・ダウンロードができるようになった。なぜかBOMが取れているが...

--- certadmin/src/bytea_api.js
+++ certadmin/src/bytea_api.js
@@ -13,16 +13,26 @@ const get_prikey_api = async function (req, res) {
     const client = new Client(client_param);
     try {
         console.log(`GET start.`);
+        console.log(req.query);
         client.connect();
 
-        const query_string = 'SELECT * FROM userfile order by file asc;';
-        const reply_object = await client.query(query_string);
+        if (typeof req.query.file_id !== 'string' && typeof req.query.file_id !== 'number'){
+            console.log('req.query.file_id is not string or number.');
+            res.status(400).send('req.query.file_id is not string or number.');
+        } else {
+            const query_string = `SELECT file_name, prikey_entity FROM certfiles WHERE file_id = ${req.query.file_id};`;
+            const reply_object = await client.query(query_string);
+            if (reply_object.rowCount === 1) {
+                res.status(200).send(reply_object.rows[0]);
+            } else {
+                res.status(400).send({"message": `failed to get file from file_id=${req.query.file_id}.`});
+            }
 
-        res.status(200).send(reply_object.rows);
-        console.log(`GET end.`);
+        }
+    console.log(`GET end.`);
     } catch (e) {
         console.log(e);
-        res.status(500).send({error: `GET API failed. detail : ${e}`});
+        res.status(500).send({error: `GET API failed.`});
 
         console.log(`API failed. detail : ${e}.\nwebadmin server restarting...`);
         process.exit(1); // force restart by docker
@@ -39,14 +49,14 @@ const post_prikey_api = async function (req, res) {
         client.connect();
 
         if (typeof req.body.file_id !== 'string' && typeof req.body.file_id !== 'number'){
-            console.log('req.body.file is not string or number.');
+            console.log('req.body.file_id is not string or number.');
+            res.status(400).send('req.body.file_id is not string or number.');
         } else {
-            const query_string = `UPDATE certfiles SET prikey_entity = '${req.body.prikey_entity}' WHERE file_id = ${req.body.file_id};`;
+            const query_string = `UPDATE certfiles SET prikey_entity = '${req.body.prikey_entity}' WHERE file_id = '${req.body.file_id}';`;
             await client.query(query_string);
 
             res.status(200).send();
         }
-
         console.log(`POST end.`);
     } catch (e) {
         res.status(500).send({error: `POST API failed.`});
