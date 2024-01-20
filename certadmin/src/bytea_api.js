ea6a374164e0a9a68e3d3bace21e06fdcc09561e
stageleft <sugano-github@elder-alliance.org>
2024-01-20 11:41:37 +0900

    ファイルのアップロード・ダウンロード、公開鍵と証明書に対応

--- certadmin/src/bytea_api.js
+++ certadmin/src/bytea_api.js
@@ -75,11 +75,136 @@ const post_prikey_api = async function (req, res) {
     }
 };
 
-const get_pubkey_api = async function (req, res) {};
-const post_pubkey_api = async function (req, res) {};
+const get_pubkey_api = async function (req, res)  {
+    const client = new Client(client_param);
+    try {
+        console.log(`GET start.`);
+        console.log(req.query);
+        client.connect();
 
-const get_cert_api = async function (req, res) {};
-const post_cert_api = async function (req, res) {};
+        if (typeof req.query.file_id !== 'string' && typeof req.query.file_id !== 'number'){
+            console.log('req.query.file_id is not string or number.');
+            res.status(400).send('req.query.file_id is not string or number.');
+        } else {
+            const query_string = `SELECT file_name, pubkey_entity FROM certfiles WHERE file_id = ${req.query.file_id};`;
+            const reply_object = await client.query(query_string);
+            if (reply_object.rowCount === 1) {
+                console.log(reply_object.rows[0])
+                res.status(200).send(reply_object.rows[0]);
+            } else {
+                res.status(400).send({"message": `failed to get file from file_id=${req.query.file_id}.`});
+            }
+
+        }
+        console.log(`GET end.`);
+    } catch (e) {
+        res.status(500).send({error: `GET API failed.`});
+
+        console.log(`GET API failed.`);
+        console.log(e);
+    } finally {
+        client.end();
+    }
+};
+const post_pubkey_api = async function (req, res) {
+    const client = new Client(client_param);
+    try {
+        console.log(`POST start.`);
+        console.log(req.body);
+        client.connect();
+
+        if (typeof req.body.file_id !== 'string' && typeof req.body.file_id !== 'number'){
+            console.log('req.body.file_id is not string or number.');
+            res.status(400).send('req.body.file_id is not string or number.');
+        } else {
+            // decode Uint8Array -> Binary
+            const entity1 = req.body.pubkey_entity.toString();
+            const entity2 = JSON.parse(`[${entity1}]`);
+            const entity3 = new Uint8Array(entity2);
+            const prikey_blob = new Blob([entity3]);
+            const pubkey_entity = Buffer.from(await prikey_blob.arrayBuffer());
+            // query
+            const query_string = `UPDATE certfiles SET pubkey_entity = '${pubkey_entity}' WHERE file_id = '${req.body.file_id}';`;
+            await client.query(query_string);
+
+            res.status(200).send();
+        }
+        console.log(`POST end.`);
+    } catch (e) {
+        res.status(500).send({error: `POST API failed.`});
+
+        console.log(`POST API failed.`);
+        console.log(e);
+    } finally {
+        client.end();
+    }
+};
+
+const get_cert_api = async function (req, res) {
+    const client = new Client(client_param);
+    try {
+        console.log(`GET start.`);
+        console.log(req.query);
+        client.connect();
+
+        if (typeof req.query.file_id !== 'string' && typeof req.query.file_id !== 'number'){
+            console.log('req.query.file_id is not string or number.');
+            res.status(400).send('req.query.file_id is not string or number.');
+        } else {
+            const query_string = `SELECT file_name, cert_entity FROM certfiles WHERE file_id = ${req.query.file_id};`;
+            const reply_object = await client.query(query_string);
+            if (reply_object.rowCount === 1) {
+                console.log(reply_object.rows[0])
+                res.status(200).send(reply_object.rows[0]);
+            } else {
+                res.status(400).send({"message": `failed to get file from file_id=${req.query.file_id}.`});
+            }
+
+        }
+        console.log(`GET end.`);
+    } catch (e) {
+        res.status(500).send({error: `GET API failed.`});
+
+        console.log(`GET API failed.`);
+        console.log(e);
+    } finally {
+        client.end();
+    }
+};
+const post_cert_api = async function (req, res) {
+    const client = new Client(client_param);
+    try {
+        console.log(`POST start.`);
+        console.log(req.body);
+        client.connect();
+
+        if (typeof req.body.file_id !== 'string' && typeof req.body.file_id !== 'number'){
+            console.log('req.body.file_id is not string or number.');
+            res.status(400).send('req.body.file_id is not string or number.');
+        } else {
+            // decode Uint8Array -> Binary
+            const entity1 = req.body.cert_entity.toString();
+            const entity2 = JSON.parse(`[${entity1}]`);
+            const entity3 = new Uint8Array(entity2);
+            const prikey_blob = new Blob([entity3]);
+            const cert_entity = Buffer.from(await prikey_blob.arrayBuffer());
+            // query
+            const query_string = `UPDATE certfiles SET cert_entity = '${cert_entity}' WHERE file_id = '${req.body.file_id}';`;
+            await client.query(query_string);
+
+            res.status(200).send();
+        }
+        console.log(`POST end.`);
+    } catch (e) {
+        res.status(500).send({error: `POST API failed.`});
+
+        console.log(`POST API failed.`);
+        console.log(e);
+    } finally {
+        client.end();
+    }
+};
+
 
 // exports
 module.exports = {
